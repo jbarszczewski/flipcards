ae73915b8d2f3a6073d5456dd55b3c7327078f5f
jbarszczewski <jakub.barszczewski@outlook.com>
2024-09-30 15:23:26 +0200

    display extra text on card

--- src/features/cards/components/card.tsx
+++ src/features/cards/components/card.tsx
@@ -16,7 +16,22 @@ const Card: React.FC<CardProps> = ({ card, updateCard, removeCard }) => {
       <div className="card-body">
         <h2 className="card-title">{card.frontText}</h2>
         <p>{card.backText}</p>
-        <p>{card.id}</p>
+        {card.extraText && (
+          <div role="alert" className="alert">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="stroke-info h-6 w-6 shrink-0">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+            </svg>
+            <p>{card.extraText}</p>
+          </div>
+        )}
         <div className="card-actions justify-end">
           <button
             className="btn btn-secondary"
